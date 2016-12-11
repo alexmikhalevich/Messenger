82503daea61b6a09ce2a9883e605381fde545c1e
maked0n <alex.mikhalevich@gmail.com>
2016-12-11 08:32:09 +0300

    Minor fixes

--- Messenger/MainWindow.xaml.cs
+++ Messenger/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,13 +22,13 @@ namespace Messenger {
     /// </summary>
     public partial class MainWindow : Window {
         private CModel m_model;
-        private const long MAX_FILE_SIZE = 209715200; //200 Mb
+        private const long MAX_FILE_SIZE = 209715200; //200 MB
         public MainWindow() {
             InitializeComponent();
             m_model = new CModel();
         }
 
-        private void LoginWindow(out string user_id, out string password, out string server_address, out ushort port, out bool use_encryption) {
+        private void _LoginWindow(out string user_id, out string password, out string server_address, out ushort port, out bool use_encryption) {
             LoginWindow window = new LoginWindow();
             window.ShowDialog();
             user_id = window.user_id;
@@ -36,22 +37,37 @@ namespace Messenger {
             port = window.port;
             use_encryption = window.encryption_enabled;
         }
+        private void _AppendText(string message, string color) {
+            BrushConverter bc = new BrushConverter();
+            TextRange tr = new TextRange(output_textbox.Document.ContentEnd, output_textbox.Document.ContentEnd);
+            tr.Text = message;
+            try {
+                tr.ApplyPropertyValue(TextElement.ForegroundProperty, bc.ConvertFromString(color));
+            } catch (FormatException) { }
+        }
+        private void _ShowMessage(string message, bool own_message) {
+            _AppendText(m_model.m_user_id + ": ", "Red");
+            _AppendText(message + "\r", "Black");
+        }
         private void Send_Click(object sender, RoutedEventArgs e) {
             if (!m_model.m_is_logged_in) {
                 string user_id, password, server_address;
                 ushort port;
                 bool use_encryption;
-                LoginWindow(out user_id, out password, out server_address, out port, out use_encryption);
+                _LoginWindow(out user_id, out password, out server_address, out port, out use_encryption);
                 m_model.Login(user_id, password, server_address, port, use_encryption);
                 if(m_model.m_is_logged_in) {
                     MessengerWindow.login_button.Content = "Send";
                     MessengerWindow.send_file_button.IsEnabled = true;
+                    MessengerWindow.message_input_textbox.IsEnabled = true;
                 }
             }
             else {
+                _ShowMessage(this.message_input_textbox.Text, true);
                 byte[] message = System.Text.Encoding.UTF8.GetBytes(this.message_input_textbox.Text);
                 m_model.SendMessage(ref message, CModel.EMessageType.Text);
             }
+            this.message_input_textbox.Clear();
         }
 
         private void Attach_Click(object sender, RoutedEventArgs e) {
@@ -75,6 +91,14 @@ namespace Messenger {
                     message_type = CModel.EMessageType.Video;
                 byte[] file_content = File.ReadAllBytes(filename);
                 m_model.SendMessage(ref file_content, message_type);
+            }
+        }
+        private void message_input_textbox_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter && !Keyboard.IsKeyDown(Key.LeftCtrl)) {
+                this.send_file_button.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+            }
+            else if (e.Key == Key.Enter && Keyboard.IsKeyDown(Key.LeftCtrl)) {
+                message_input_textbox.Text += "\r";
             }
         }
     }
